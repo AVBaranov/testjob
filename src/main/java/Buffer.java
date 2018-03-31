424df4142e3efcffcaeabcfe9fe6031262d84b86
Andrey Baranov <tread_lightly@list.ru>
2018-03-31 19:09:54 +1000

    create object

---
+++ src/main/java/Buffer.java
@@ -0,0 +1,15 @@
+package concat;
+
+public class Buffer {
+    private StringBuffer buffer = new StringBuffer();
+
+    public void add(int value) {
+        System.out.print(value);
+        this.buffer.append(value);
+    }
+
+    @Override
+    public String toString() {
+        return this.buffer.toString();
+    }
+}
